d28decfa6ef2efa08f2c9f305097714178670b46
roylaurie <roy.laurie@asimovian.software>
2016-07-20 15:53:18 -0700

    testing

--- install-frog.bash
+++ install-frog.bash
@@ -1,68 +1,68 @@
 #!/bin/bash
 set -xoue pipefail
-IFS=$'\n\t'
+IFS=$'${NC}\n\t'
 
 #+--------------------------------------+--------------------------------------+
 GRN='\033[1;32m'
-echo -e "${GRN}+----------------------------------bullfrog-----------------------------------+\n"
-echo -e "${GRN}+-----------------------------------system------------------------------------+\n"
-echo -e "${GRN}|                                                                             |\n"
+echo -e "${GRN}+----------------------------------bullfrog-----------------------------------+${NC}\n"
+echo -e "${GRN}+-----------------------------------system------------------------------------+${NC}\n"
+echo -e "${GRN}|                                                                             |${NC}\n"
 
-echo -e "${GRN}|=== Updating system packages ...                                             |\n"
+echo -e "${GRN}|=== Updating system packages ...                                             |${NC}\n"
 sudo apt-get update
 sudo apt-get upgrade -y
 
-echo -e "${GRN}|=== Updating system packates ...                                             |\n"
+echo -e "${GRN}|=== Updating system packates ...                                             |${NC}\n"
 sudo apt-get install boost cmake git openssl autoconf automake qt5 ncurses readline doxygen\
  ncurses-dev graphviz readline libreadline-dev doxygen ncurses-dev graphviz libreadline6\
  libreadline6-dev libgmp-dev zip unzip nodejs
 
-echo -e "${GRN}|=== Creating user 'frog' ...                                                 |\n"
+echo -e "${GRN}|=== Creating user 'frog' ...                                                 |${NC}\n"
 sudo adduser frog
 
-echo -e "${GRN}|=== Logging in as user 'frog' ...                                            |\n"
+echo -e "${GRN}|=== Logging in as user 'frog' ...                                            |${NC}\n"
 sudo su frog
 
-echo -e "${GRN}|=== Cloning 'bullfrog-system' project ...                                    |\n"
+echo -e "${GRN}|=== Cloning 'bullfrog-system' project ...                                    |${NC}\n"
 mkdir -p /home/frog/project
 cd /home/frog/project
 git clone https://github.com/roylaurie/bullfrog-system.git
 
-echo -e "${GRN}|=== Restricting file permissions in home dir ...                             |\n"
+echo -e "${GRN}|=== Restricting file permissions in home dir ...                             |${NC}\n"
 chown -R frog:frog /home/frog
 chmod -R o-rwx /home/frog
 
-echo -e "${GRN}|=== Logging out as 'frog' ...                                                |\n"
+echo -e "${GRN}|=== Logging out as 'frog' ...                                                |${NC}\n"
 exit # frog
 
-echo -e "${GRN}|=== Configuring system service 'STEEM DAEMON' ...                            |\n"
+echo -e "${GRN}|=== Configuring system service 'STEEM DAEMON' ...                            |${NC}\n"
 sudo cp /home/frog/project/bullfrog-system/fs/etc/systemd/system/steemd.service /etc/systemd/system
 sudo chown root:root /etc/systemd/system/steemd.service
 sudo systemctl daemon-reload
 
 
-echo -e "${GRN}|=== Building STEEM project ...                                               |\n"
+echo -e "${GRN}|=== Building STEEM project ...                                               |${NC}\n"
 sudo /home/frog/project/bullfrog-system/bin/recompile-steem.bash
 
-echo -e "${GRN}|=== Configuring steemd for 'synconly' ...                                    |\n"
+echo -e "${GRN}|=== Configuring steemd for 'synconly' ...                                    |${NC}\n"
 sudo /home/frog/project/bullfrog-system/bin/config-steemd.bash synconly
 
-echo -e "${GRN}|=== Downloading snapshot of steemd blockchain database ...                   |\n"
+echo -e "${GRN}|=== Downloading snapshot of steemd blockchain database ...                   |${NC}\n"
 # TODO
-echo -e "${GRN}|=== Importing snapshot of steemd blockchain database ...                     |\n"
+echo -e "${GRN}|=== Importing snapshot of steemd blockchain database ...                     |${NC}\n"
 # TODO
 
-echo -e "${GRN}|=== Installation complete. Starting replay. Once complete:                   |\n"
-echo -e "${GRN}|       * CTL+C to kill steemd.                                               |\n"
-echo -e "${GRN}|       * Configure files in /usr/local/var/lib/steemd/configs.               |\n"
-echo -e "${GRN}|       * sudo /home/bullfrog/project/bullfrog-system/bin/config-steemd.bash. |\n"
-echo -e "${GRN}|       * sudo service steemd start                                           |\n"
-echo -e "${GRN}|                                                                             |\n"
-echo -e "${GRN}+-----------------------------------------------------------------------------+\n"
-echo -e "${GRN}+---------------------------installation-complete-----------------------------+\n"
-echo -e "${GRN}+-----------------------------------------------------------------------------+\n"
+echo -e "${GRN}|=== Installation complete. Starting replay. Once complete:                   |${NC}\n"
+echo -e "${GRN}|       * CTL+C to kill steemd.                                               |${NC}\n"
+echo -e "${GRN}|       * Configure files in /usr/local/var/lib/steemd/configs.               |${NC}\n"
+echo -e "${GRN}|       * sudo /home/bullfrog/project/bullfrog-system/bin/config-steemd.bash. |${NC}\n"
+echo -e "${GRN}|       * sudo service steemd start                                           |${NC}\n"
+echo -e "${GRN}|                                                                             |${NC}\n"
+echo -e "${GRN}+-----------------------------------------------------------------------------+${NC}\n"
+echo -e "${GRN}+---------------------------installation-complete-----------------------------+${NC}\n"
+echo -e "${GRN}+-----------------------------------------------------------------------------+${NC}\n"
 
-echo -e "${GRN}|=== Replaying imported database in steemd  ...                              |\n"
+echo -e "${GRN}|=== Replaying imported database in steemd  ...                              |${NC}\n"
 sudo /home/frog/project/bullfrog/bin/replay.bash
 
 exit 0
