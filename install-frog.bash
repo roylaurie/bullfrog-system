9974d5f9cc5b0faa3077a06aa148d48845c1c684
roylaurie <roy.laurie@asimovian.software>
2016-07-20 16:24:18 -0700

    wip

--- install-frog.bash
+++ install-frog.bash
@@ -12,10 +12,10 @@ echo -e "${GRN}|=== Updating system packages ...                                
 sudo apt-get update
 sudo apt-get upgrade -y
 
-echo -e "${GRN}|=== Updating system packates ...                                             |${NC}"
-sudo apt-get install gcc g++ libboost-all-dev cmake openssl-dev autoconf automake qt5-default\
- qttools5-dev-tools doxygen ncurses-dev doxygen ncurses-dev graphviz libreadline6\
- libreadline6-dev libgmp-dev zip unzip nodejs
+echo -e "${GRN}|=== Installing required system packages ...                                 |${NC}"
+sudo apt-get install gcc g++ libboost-all-dev cmake autoconf automake qt5-default\
+ qttools5-dev-tools doxygen libncurses5-dev libncurses graphviz libreadline6\
+ libreadline6-dev libgmp-dev zip unzip nodejs python3
 
 echo -e "${GRN}|=== Creating user 'frog' ...                                                 |${NC}"
 sudo adduser frog
