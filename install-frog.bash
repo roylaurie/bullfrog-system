0499a45dc1a19a001ad6b1bafdbfb42274f64b88
roylaurie <roy.laurie@asimovian.software>
2016-07-21 09:58:07 -0700

    wip

--- install-frog.bash
+++ install-frog.bash
@@ -17,7 +17,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
  
 echo -e "${GRN}|=== Installing required system packages ...                                  |${NC}"
-sudo apt-get install gcc g++ libboost-all-dev cmake autoconf automake qt5-default\
+sudo apt-get -y install gcc g++ libboost-all-dev cmake autoconf automake qt5-default\
  qttools5-dev-tools doxygen libncurses5-dev libncurses5 graphviz libreadline6\
  libreadline6-dev libgmp-dev zip unzip nodejs python3 vim sysstat libssl-dev
 
